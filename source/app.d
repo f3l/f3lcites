bfcd79b288eb5eabf290ae498d619968337f2a5b
Oliver Rümpelein <oli_r@fg4f.de>
2016-03-12 19:13:14 +0100

    First succesfull working draft:
     * Add cites using "/add"
     * Get random Cite using "/random"

--- source/app.d
+++ source/app.d
@@ -1,17 +1,44 @@
 import vibe.d;
 import mysql;
+import f3lcites.cite;
+
+final class CiteSystem {
+    import std.random: uniform;
+    import std.conv;
+    private Cite[] cites;
+
+    void get(string title="Index")
+    {
+        render!("index.dt", title);
+    }
+
+    void getRandom() // ~ GET random
+    {
+        string quote = cites[(uniform(0, cites.length))].cite;
+        render!("random.dt", quote);
+    }
+
+    void getAdd(string title="Add new Quote")
+    {
+        render!("add.dt", title);
+    }
+
+    void postAdded(string cite)
+    {
+        cites ~= Cite(cite);
+        redirect("");
+    }
+}
 
 shared static this()
 {
-	auto settings = new HTTPServerSettings;
+    auto router = new URLRouter;
+    router.registerWebInterface(new CiteSystem);
+    router.get("*", serveStaticFiles("public/"));
+
+    auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
-
+	listenHTTP(settings, router);
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
-
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
 }
---
+++ views/random.d
@@ -0,0 +1,4 @@
+extends ./layout
+
+block content
+  p You may be able to view a random F3Lcite here quite soon!
