fd6244ab3e5decee5b2f1406e5866a5056a30775
Oliver Rümpelein <oli_r@fg4f.de>
2016-03-19 10:58:53 +0100

    Small cosemtic changes
     * Modified settings to match the later static port
     * random-cites are viewed within a <p>

--- source/app.d
+++ source/app.d
@@ -71,10 +71,10 @@ shared static this()
     router.get("*", serveStaticFiles("public/"));
 
     auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = 8088;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 	listenHTTP(settings, router);
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	logInfo("Please open http://127.0.0.1:" ~ settings.port ~ "/ in your browser.");
 }
 
 unittest
