496a79e137590de2633541bfb9e74dd670c9f534
Oliver Rümpelein <oli_r@fg4f.de>
2016-11-28 20:19:13 +0100

    Added simple REST interface

--- source/app.d
+++ source/app.d
@@ -1,7 +1,13 @@
 //module f3lcites;
 import vibe.d;
+import vibe.data.json;    
 import std.conv;
 import f3lcites.sqlite;
+
+struct StatusReturn {
+    int status;
+    string message;
+}
 
 final class CiteSystem {
 private:
@@ -10,6 +16,10 @@ private:
     DB db;
 
 public:
+    this(ref DB db) {
+        this.db = db;
+    }
+    
     this(string dbKey) {
         this.db = new CiteSqlite(dbKey);
     }
@@ -26,14 +36,14 @@ public:
     void getIndex() const {
         get();
     }
-    
+
     void getCite(long id) {
         string title ="Number "~id.to!string;
         FullCiteData cite = this.db.get(id);
         string desc = "Zitat Nr %d".format(id);
         render!("cite.dt", title, cite, desc);
     }
-    
+
     void getRandomPlain() {
         FullCiteData quote = this.db.getRandomCite();
         render!("random_plain.dt", quote);
@@ -63,16 +73,16 @@ public:
     void getModify(long id) {
         FullCiteData cite = this.db.get(id);
         string citetext = cite.cite;
-        string title = "Zitat Nr. %d bearbeiten".format(id); 
+        string title = "Zitat Nr. %d bearbeiten".format(id);
         render!("modify.dt", id, title, citetext);
     }
-    
+
     void postDoModify(long id, string cite, string changedby) {
         string modifiedCite = this.stripCite(cite);
         long lastId = this.db.modifyCite(id, modifiedCite, changedby);
         redirect("cite?id=%d".format(lastId));
     }
-    
+
     void postAdded(string cite, string name) {
         string addedCite = this.stripCite(cite);
         long lastId = this.db.addCite(cite, name);
@@ -89,6 +99,57 @@ public:
             .replace("\n", " – ");
         return retval;
     }
+}
+
+final class CiteApi {
+    DB db;
+
+    this(DB db) {
+        this.db = db;
+    }
+
+    void getById(HTTPServerRequest req, HTTPServerResponse resp) {
+        auto id = req.params["id"].to!long;
+        auto contentType = "application/json";
+        auto responseString = (id)
+            ? this.db.get(id).toJsonString
+            : FullCiteData.init.toJsonString;
+        resp.writeBody(responseString, contentType);
+    }
+
+    void getRandom(HTTPServerRequest req, HTTPServerResponse resp) {
+        auto contentType = "application/json";
+        auto responseString = this.db.getRandomCite.toJsonString;
+        resp.writeBody(responseString, contentType);
+    }
+
+    void addCite(HTTPServerRequest req, HTTPServerResponse resp) {
+        auto author = req.json["author"].to!string;
+        auto cite = req.json["cite"].to!string;
+        auto contentType = "application/json";
+        // Return Status JSON, write Error JSON!
+        if (author == "") {
+            resp.statusCode = 400;
+            auto responseString = StatusReturn(400, "No author set.").toJsonString;
+            resp.writeBody(responseString, contentType);
+        }
+        if (cite == "") {
+            resp.statusCode = 400;
+            auto responseString = StatusReturn(400, "No cite set.").toJsonString;
+            resp.writeBody(responseString, contentType);
+        }
+        assert(author.length != 0 && cite.length != 0);
+        long addedId = db.addCite(cite, author);
+        if (addedId <= 0) {
+            resp.statusCode = 500;
+            auto responseString = StatusReturn(500, "Cite not added internally").toJsonString;
+            resp.writeBody(responseString, contentType);
+        } else {
+            auto responseString = StatusReturn(200, "Added with ID %s".format(addedId)).toJsonString;
+            resp.writeBody(responseString, contentType);
+        }
+    }
+                 
 }
 
 shared static this() {
@@ -110,14 +171,22 @@ shared static this() {
     string dbPath;
     readOption("d|dbpath", &dbPath, "Path to SQLite DB");
 
-    
+    DB db;
+    if (dbPath) {
+        db = new CiteSqlite(dbPath);
+    } else {
+        db = new CiteSqlite();
+    }
     
     // Web-Routing
     auto router = new URLRouter;
-    router.registerWebInterface(
-        (dbPath)
-        ? new CiteSystem(dbPath)
-        : new CiteSystem);
+    auto webInterface = new CiteSystem(db);
+    router.registerWebInterface(webInterface);
+    auto restInterface = new CiteApi(db);
+    router.get("/api/get", &(restInterface.getRandom));
+    router.get("/api/get/:id", &(restInterface.getById));
+    router.post("/api/add", &(restInterface.addCite));                              
+    
     listenHTTP(settings, router);
 
     logInfo("Please open http://"
--- source/citedata.d
+++ source/citedata.d
@@ -1,6 +1,8 @@
 module f3lcites.citedata;
 public import std.datetime;
 
+import vibe.data.json;
+
 struct FullCiteData {
     long id;
     string cite;
@@ -8,4 +10,14 @@ struct FullCiteData {
     string addedby;
     Date changed;
     string changedby;
+
 }
+
+Json toJson(T)(T input) {
+    return input.serializeToJson();
+}
+
+string toJsonString(T)(T input) {
+    return input.toJson.to!string;
+}
+
