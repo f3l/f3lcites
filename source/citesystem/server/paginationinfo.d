c41157520e966b8c46815c46c611fe8e9a96627a
Oliver Rümpelein <oli_r@fg4f.de>
2019-01-05 17:19:48 +0100

    Add pagination markers and ellipses dynamically

--- source/citesystem/server/paginationinfo.d
+++ source/citesystem/server/paginationinfo.d
@@ -1,5 +1,7 @@
 module citesystem.server.paginationinfo;
 
+private import diet.dom : Node;
+
 public struct PaginationInfo {
     const size_t pagesize;
     const size_t currentPage;
@@ -12,7 +14,7 @@ public struct PaginationInfo {
     }
 
     @property
-    const size_t lastPage() {
+    size_t lastPage() const {
         auto numberOfFullPages = numberOfElements / pagesize;
         if (numberOfFullPages % pagesize == 0) {
             return numberOfFullPages + 1;
@@ -22,29 +24,66 @@ public struct PaginationInfo {
     }
 
     @property
-    const size_t[] pagesToShow() {
-        import std.algorithm.comparison : min, max;
+    size_t[] pagesToShow() const {
         import std.range : iota;
         import std.array : array;
 
-        auto firstPageLabel = (currentPage > 3) ? currentPage - 3 : 1;
+        auto firstPageLabel = firstPageLabel();
         // assert(firstPageLabel > 0);
-        auto lastPageLabel = min(currentPage + 3, lastPage);
+        auto lastPageLabel = lastPageLabel();
         // assert(lastPageLabel <= lastPage);
         return iota(firstPageLabel, lastPageLabel + 1).array();
     }
 
+    private ulong firstPageLabel() const {
+        return (currentPage > 3) ? currentPage - 3 : 1;
+    }
+
+    private ulong lastPageLabel() const {
+        import std.algorithm.comparison : min;
+        return min(currentPage + 3, lastPage);
+    }
+
     @property
-    const size_t firstCiteOffset() {
+    size_t firstCiteOffset() const {
         return (pagesize * (currentPage - 1));
     }
 
     @property
-    const size_t lastCite() {
+    size_t lastCite() const {
         import std.algorithm.comparison : min;
         return min(
             pagesize * currentPage,
             numberOfElements
         );
+    }
+
+    @property
+    Node[] allMarks() const {
+        import diet.parser : parseDietRaw, identity;
+        import diet.input : InputFile;
+        InputFile inputFile = InputFile("pageinationInfo-footer", q{a(href="https://pheerai.de/") HP});
+
+        return parseDietRaw!identity(inputFile);
+    }
+
+    @property
+    bool needsFrontTruncation() const {
+        return firstPageLabel != 1;
+    }
+
+    @property
+    bool needsBackTruncation() const {
+        return lastPageLabel != lastPage;
+    }
+
+    @property
+    bool needsNextPage() const {
+        return currentPage != lastCite;
+    }
+
+    @property
+    bool needsPreviousPage() const {
+        return currentPage != 1;
     }
 }
